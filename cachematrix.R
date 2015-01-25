7fd7bdfcb7afb1dd02a409c1cf0ca85e06c16958
Ahmed Abdel-Aaty <ahmed.abdelaaty@gmail.com>
2015-01-25 17:13:43 -0500

    Implemented assignment functions
    Implemented makeCacheMatrix and cacheSolve functions as per assignment
    requirements

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program contains a pair of functions that facilitate creating a matrix object and solving for it
+## while utilizing the caching mechanism in R.
 
-## Write a short comment describing this function
+## makeCacheMatrix - Takes a matrix as an argument and builds the supporting functions for caching
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverse <- NULL
+	set <- function(y) {
+		x <<- y
+		inverse <<- NULL
+	}
+	get <- function() x
+	setInverse <- function(inv) inverse <<- inv
+	getInverse <- function() inverse
+	list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - Takes a first argument 'x' which is a cached matrix and solves it. It will use the cached value if available. The additional arguments
+## are passed to the solve function if they are provided.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	inverse <- x$getInverse()
+	# Check if a cached value exists
+	if(!is.null(inverse)) {
+		message("Returning cached data.")
+		return(inverse)
+	}
+	# No cached value found, proceed with the calculation
+	data <- x$get()
+	inverse <- solve(data,...)
+	x$setInverse(inverse)
+	inverse
 }
